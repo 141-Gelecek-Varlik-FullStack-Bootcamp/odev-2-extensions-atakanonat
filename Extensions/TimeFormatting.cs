4bce5bdd6fe75118e22210dd5ed3d5c6333f03b6
atakanonat <atakanonat628@gmail.com>
2021-12-03 15:36:56 +0300

    first commit

---
+++ Extensions/TimeFormatting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Extensions
+{
+    public static class TimeFormatting
+    {
+        public static DateTime ToTurkeyTime(this DateTime dateTime)
+        {
+            dateTime = dateTime.ToUniversalTime();
+            TimeZoneInfo turkeyZone = TimeZoneInfo.FindSystemTimeZoneById("Turkey");
+            DateTime turkeyTime = TimeZoneInfo.ConvertTimeFromUtc(dateTime, turkeyZone);
+            return turkeyTime;
+        }
+    }
+}
